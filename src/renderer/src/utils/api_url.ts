babfa3b11a2a1818fa2eb431ba5cf583edab3438
lincon07 <peaklincoln5@gmail.com>
2026-01-01 16:56:56 -0800

    api url

--- src/renderer/src/utils/api_url.ts
+++ src/renderer/src/utils/api_url.ts
@@ -1,1 +1,1 @@
-export const API_BASE = import.meta.env.DEV ? "192.168.1.105:3008" : "https://solora-api-841c6cc58685.herokuapp.com"
+export const API_BASE = import.meta.env.DEV ? "http://192.168.1.105:3008" : "https://solora-api-841c6cc58685.herokuapp.com"
