ed697f0d462d12656ec659a6023346560ac55dac
lincon07 <peaklincoln5@gmail.com>
2026-01-01 16:09:41 -0800

    testing new keyboard cmd

--- src/main/index.ts
+++ src/main/index.ts
@@ -96,8 +96,8 @@ function createWindow(): void {
 
   mainWindow.on('ready-to-show', () => {
     mainWindow?.show()
-    mainWindow?.focus()
-    mainWindow?.maximize()
+    // mainWindow?.focus()
+    // mainWindow?.maximize()
   })
 
   mainWindow.webContents.setWindowOpenHandler(details => {
